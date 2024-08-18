d0d43f596a7e6041e2a8f42cb3a813728c3f3fec
sachin-jodo <sachin.singh@jodopay.com>
2024-08-18 17:26:43 +0000

    added barcode gen, big box

---
+++ torzel_custom/torzel_custom/doctype/big_box/test_big_box.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, V12 Infotech and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestBigBox(FrappeTestCase):
+	pass
