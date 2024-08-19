e70893b249ca0e22d78200b3185a4e319913637e
sachin-jodo <sachin.singh@jodopay.com>
2024-08-19 18:27:18 +0000

    added get weight from serial device

--- torzel_custom/torzel_custom/doctype/barcode_generator/barcode_generator.js
+++ torzel_custom/torzel_custom/doctype/barcode_generator/barcode_generator.js
@@ -1,8 +1,47 @@
 // Copyright (c) 2024, V12 Infotech and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Barcode Generator", {
-// 	refresh(frm) {
 
-// 	},
-// });
+frappe.ui.form.on("Barcode Generator", {
+    refresh(frm) {
+        frm.add_custom_button(__('Capture Weight'), async function () {
+            if ("serial" in navigator) {
+                // The Web Serial API is supported.
+                // Prompt user to select any serial port.
+                const port = await navigator.serial.requestPort();
+                // Get all serial ports the user has previously granted the website access to.
+                const ports = await navigator.serial.getPorts();
+
+                console.log({ port, ports })
+
+                // Wait for the serial port to open.
+                await port.open({ baudRate: 9600 });
+
+                const textDecoder = new TextDecoderStream();
+                const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
+                const reader = textDecoder.readable.getReader();
+
+                // Listen to data coming from the serial device.
+                while (true) {
+                    const { value, done } = await reader.read();
+                    if (done) {
+                        // Allow the serial port to be closed later.
+                        reader.releaseLock();
+                        break;
+                    }
+                    // value is a string.
+                    console.log(value);
+                    // Set the captured weight in the weight field
+                    frm.set_value('gross_weight', value);
+                    frappe.msgprint(__('Weight captured: ') + value + ' kg');
+                }
+            } else {
+                frappe.msgprint({
+                    message: __("Please use google chrome browser"),
+                    title: __("Web serial api is not supported."),
+                    indicator: "red",
+                });
+            }
+        });
+    }
+});
--- torzel_custom/torzel_custom/doctype/barcode_generator/barcode_generator.py
+++ torzel_custom/torzel_custom/doctype/barcode_generator/barcode_generator.py
@@ -1,9 +1,6 @@
 # Copyright (c) 2024, V12 Infotech and contributors
 # For license information, please see license.txt
 
-# import frappe
 from frappe.model.document import Document
-
-
 class BarcodeGenerator(Document):
-	pass
+    pass
