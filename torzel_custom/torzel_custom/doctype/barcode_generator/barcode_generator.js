1c0dad9ff8810f18d9f13304a3339885d880ecc1
sachin-jodo <sachin.singh@jodopay.com>
2024-08-21 15:38:46 +0000

    changed capture weight login

--- torzel_custom/torzel_custom/doctype/barcode_generator/barcode_generator.js
+++ torzel_custom/torzel_custom/doctype/barcode_generator/barcode_generator.js
@@ -4,44 +4,58 @@
 
 frappe.ui.form.on("Barcode Generator", {
     refresh(frm) {
-        frm.add_custom_button(__('Capture Weight'), async function () {
-            if ("serial" in navigator) {
-                // The Web Serial API is supported.
-                // Prompt user to select any serial port.
-                const port = await navigator.serial.requestPort();
+
+        // Remove any existing buttons with the same label (optional cleanup)
+        frm.clear_custom_buttons();
+
+        if ("serial" in navigator) {
+            // The Web Serial API is supported.
+            (async () => {
                 // Get all serial ports the user has previously granted the website access to.
                 const ports = await navigator.serial.getPorts();
+                let port = null;
 
-                console.log({ port, ports })
-
-                // Wait for the serial port to open.
-                await port.open({ baudRate: 9600 });
-
-                const textDecoder = new TextDecoderStream();
-                const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
-                const reader = textDecoder.readable.getReader();
-
-                // Listen to data coming from the serial device.
-                while (true) {
-                    const { value, done } = await reader.read();
-                    if (done) {
-                        // Allow the serial port to be closed later.
-                        reader.releaseLock();
-                        break;
-                    }
-                    // value is a string.
-                    console.log(value);
-                    // Set the captured weight in the weight field
-                    frm.set_value('gross_weight', value);
-                    frappe.msgprint(__('Weight captured: ') + value + ' kg');
+                if (ports.length) {
+                    port = ports[0];
+                } else {
+                    frm.add_custom_button(__('Connect To Weight Machine'), async function () {
+                        // Prompt user to select any serial port.
+                        port = await navigator.serial.requestPort();
+                    });
                 }
-            } else {
-                frappe.msgprint({
-                    message: __("Please use google chrome browser"),
-                    title: __("Web serial api is not supported."),
-                    indicator: "red",
+
+                frm.add_custom_button(__('Capture Weight'), async function () {
+                    // Wait for the serial port to open.
+                    await port.open({ baudRate: 9600 });
+
+                    const textDecoder = new TextDecoderStream();
+                    const reader = textDecoder.readable.getReader();
+                    // Listen to data coming from the serial device.
+                    while (true) {
+                        const { value, done } = await reader.read();
+                        if (done) {
+                            // Allow the serial port to be closed later.
+                            reader.releaseLock();
+                            break;
+                        }
+                        // value is a string.
+                        console.log(value);
+                        // Set the captured weight in the weight field
+                        frm.set_value('gross_weight', value);
+                        frappe.msgprint(__('Weight captured: ') + value + ' kg');
+                    }
                 });
-            }
-        });
+
+                frm.add_custom_button(__('Connect to another machine'), async function () {
+                    port = await navigator.serial.requestPort();
+                });
+            })()
+        } else {
+            frappe.msgprint({
+                message: __("Please use google chrome browser"),
+                title: __("Web serial api is not supported."),
+                indicator: "red",
+            });
+        }
     }
 });
